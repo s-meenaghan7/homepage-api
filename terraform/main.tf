264145e08640f39957688c8041e6cd4e43e0b920
Sean Meenaghan <seanmeenaghan@gmail.com>
2026-01-16 17:16:04 -0800

    feat: terraform state backend properly configured

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,14 @@ terraform {
       version = "~> 6"
     }
   }
+
+  backend "s3" {
+    bucket       = "s-meenaghan-terraform-state"
+    key          = "homepage-api/terraform.tfstate"
+    region       = "us-west-2"
+    encrypt      = true
+    use_lockfile = true
+  }
 }
 
 provider "aws" {
